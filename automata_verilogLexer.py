b543c22f1f164b4f5181800b832563f4ae02b26f
403page <49970397+403page@users.noreply.github.com>
2020-02-11 15:34:18 +0800

    Create automata_verilogLexer.py

---
+++ automata_verilogLexer.py
@@ -0,0 +1,164 @@
+# Generated from automata_verilog.g4 by ANTLR 4.7.2
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\"")
+        buf.write("\u00d1\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\4\35\t\35\4\36")
+        buf.write("\t\36\4\37\t\37\4 \t \4!\t!\3\2\3\2\3\3\3\3\3\4\3\4\3")
+        buf.write("\5\3\5\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3\t\3\t\3\t\3\t\3\t")
+        buf.write("\3\t\3\n\3\n\3\13\3\13\3\13\3\f\3\f\3\f\3\f\3\f\3\f\3")
+        buf.write("\f\3\f\3\r\3\r\3\r\3\r\3\r\3\r\3\16\3\16\3\16\3\16\3\17")
+        buf.write("\3\17\3\17\3\20\3\20\3\20\3\20\3\20\3\21\3\21\3\21\3\22")
+        buf.write("\3\22\3\22\3\23\3\23\3\24\3\24\3\25\3\25\3\26\3\26\3\27")
+        buf.write("\3\27\3\30\3\30\3\30\3\30\3\30\3\30\3\30\3\31\3\31\3\31")
+        buf.write("\3\31\3\31\3\31\3\31\3\31\3\31\3\31\3\32\3\32\3\32\3\32")
+        buf.write("\3\32\3\32\3\32\3\32\3\32\3\32\3\32\3\32\3\32\3\32\3\32")
+        buf.write("\3\32\3\32\3\32\3\32\3\32\3\32\3\32\3\32\5\32\u00b0\n")
+        buf.write("\32\3\33\3\33\3\33\3\33\3\33\3\33\3\33\3\34\3\34\3\35")
+        buf.write("\3\35\3\36\6\36\u00be\n\36\r\36\16\36\u00bf\3\37\6\37")
+        buf.write("\u00c3\n\37\r\37\16\37\u00c4\3 \3 \3 \3 \3!\6!\u00cc\n")
+        buf.write("!\r!\16!\u00cd\3!\3!\2\2\"\3\3\5\4\7\5\t\6\13\7\r\b\17")
+        buf.write("\t\21\n\23\13\25\f\27\r\31\16\33\17\35\20\37\21!\22#\23")
+        buf.write("%\24\'\25)\26+\27-\30/\31\61\32\63\33\65\34\67\359\36")
+        buf.write(";\37= ?!A\"\3\2\5\3\2\62;\b\2##\61;C\\^^aac|\5\2\13\f")
+        buf.write("\17\17\"\"\2\u00d7\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2")
+        buf.write("\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21")
+        buf.write("\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3")
+        buf.write("\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2!\3\2\2")
+        buf.write("\2\2#\3\2\2\2\2%\3\2\2\2\2\'\3\2\2\2\2)\3\2\2\2\2+\3\2")
+        buf.write("\2\2\2-\3\2\2\2\2/\3\2\2\2\2\61\3\2\2\2\2\63\3\2\2\2\2")
+        buf.write("\65\3\2\2\2\2\67\3\2\2\2\29\3\2\2\2\2;\3\2\2\2\2=\3\2")
+        buf.write("\2\2\2?\3\2\2\2\2A\3\2\2\2\3C\3\2\2\2\5E\3\2\2\2\7G\3")
+        buf.write("\2\2\2\tI\3\2\2\2\13K\3\2\2\2\rM\3\2\2\2\17O\3\2\2\2\21")
+        buf.write("Q\3\2\2\2\23X\3\2\2\2\25Z\3\2\2\2\27]\3\2\2\2\31e\3\2")
+        buf.write("\2\2\33k\3\2\2\2\35o\3\2\2\2\37r\3\2\2\2!w\3\2\2\2#z\3")
+        buf.write("\2\2\2%}\3\2\2\2\'\177\3\2\2\2)\u0081\3\2\2\2+\u0083\3")
+        buf.write("\2\2\2-\u0085\3\2\2\2/\u0087\3\2\2\2\61\u008e\3\2\2\2")
+        buf.write("\63\u00af\3\2\2\2\65\u00b1\3\2\2\2\67\u00b8\3\2\2\29\u00ba")
+        buf.write("\3\2\2\2;\u00bd\3\2\2\2=\u00c2\3\2\2\2?\u00c6\3\2\2\2")
+        buf.write("A\u00cb\3\2\2\2CD\7*\2\2D\4\3\2\2\2EF\7+\2\2F\6\3\2\2")
+        buf.write("\2GH\7.\2\2H\b\3\2\2\2IJ\7]\2\2J\n\3\2\2\2KL\7<\2\2L\f")
+        buf.write("\3\2\2\2MN\7_\2\2N\16\3\2\2\2OP\7?\2\2P\20\3\2\2\2QR\7")
+        buf.write("c\2\2RS\7n\2\2ST\7y\2\2TU\7c\2\2UV\7{\2\2VW\7u\2\2W\22")
+        buf.write("\3\2\2\2XY\7B\2\2Y\24\3\2\2\2Z[\7q\2\2[\\\7t\2\2\\\26")
+        buf.write("\3\2\2\2]^\7r\2\2^_\7q\2\2_`\7u\2\2`a\7g\2\2ab\7f\2\2")
+        buf.write("bc\7i\2\2cd\7g\2\2d\30\3\2\2\2ef\7d\2\2fg\7g\2\2gh\7i")
+        buf.write("\2\2hi\7k\2\2ij\7p\2\2j\32\3\2\2\2kl\7g\2\2lm\7p\2\2m")
+        buf.write("n\7f\2\2n\34\3\2\2\2op\7k\2\2pq\7h\2\2q\36\3\2\2\2rs\7")
+        buf.write("g\2\2st\7n\2\2tu\7u\2\2uv\7g\2\2v \3\2\2\2wx\7?\2\2xy")
+        buf.write("\7?\2\2y\"\3\2\2\2z{\7>\2\2{|\7?\2\2|$\3\2\2\2}~\7\u0080")
+        buf.write("\2\2~&\3\2\2\2\177\u0080\7~\2\2\u0080(\3\2\2\2\u0081\u0082")
+        buf.write("\7(\2\2\u0082*\3\2\2\2\u0083\u0084\7}\2\2\u0084,\3\2\2")
+        buf.write("\2\u0085\u0086\7\177\2\2\u0086.\3\2\2\2\u0087\u0088\7")
+        buf.write("o\2\2\u0088\u0089\7q\2\2\u0089\u008a\7f\2\2\u008a\u008b")
+        buf.write("\7w\2\2\u008b\u008c\7n\2\2\u008c\u008d\7g\2\2\u008d\60")
+        buf.write("\3\2\2\2\u008e\u008f\7g\2\2\u008f\u0090\7p\2\2\u0090\u0091")
+        buf.write("\7f\2\2\u0091\u0092\7o\2\2\u0092\u0093\7q\2\2\u0093\u0094")
+        buf.write("\7f\2\2\u0094\u0095\7w\2\2\u0095\u0096\7n\2\2\u0096\u0097")
+        buf.write("\7g\2\2\u0097\62\3\2\2\2\u0098\u0099\7k\2\2\u0099\u009a")
+        buf.write("\7p\2\2\u009a\u009b\7r\2\2\u009b\u009c\7w\2\2\u009c\u00b0")
+        buf.write("\7v\2\2\u009d\u009e\7q\2\2\u009e\u009f\7w\2\2\u009f\u00a0")
+        buf.write("\7v\2\2\u00a0\u00a1\7r\2\2\u00a1\u00a2\7w\2\2\u00a2\u00b0")
+        buf.write("\7v\2\2\u00a3\u00a4\7k\2\2\u00a4\u00a5\7p\2\2\u00a5\u00a6")
+        buf.write("\7q\2\2\u00a6\u00a7\7w\2\2\u00a7\u00b0\7v\2\2\u00a8\u00a9")
+        buf.write("\7y\2\2\u00a9\u00aa\7k\2\2\u00aa\u00ab\7t\2\2\u00ab\u00b0")
+        buf.write("\7g\2\2\u00ac\u00ad\7t\2\2\u00ad\u00ae\7g\2\2\u00ae\u00b0")
+        buf.write("\7i\2\2\u00af\u0098\3\2\2\2\u00af\u009d\3\2\2\2\u00af")
+        buf.write("\u00a3\3\2\2\2\u00af\u00a8\3\2\2\2\u00af\u00ac\3\2\2\2")
+        buf.write("\u00b0\64\3\2\2\2\u00b1\u00b2\7c\2\2\u00b2\u00b3\7u\2")
+        buf.write("\2\u00b3\u00b4\7u\2\2\u00b4\u00b5\7k\2\2\u00b5\u00b6\7")
+        buf.write("i\2\2\u00b6\u00b7\7p\2\2\u00b7\66\3\2\2\2\u00b8\u00b9")
+        buf.write("\7\60\2\2\u00b98\3\2\2\2\u00ba\u00bb\7=\2\2\u00bb:\3\2")
+        buf.write("\2\2\u00bc\u00be\t\2\2\2\u00bd\u00bc\3\2\2\2\u00be\u00bf")
+        buf.write("\3\2\2\2\u00bf\u00bd\3\2\2\2\u00bf\u00c0\3\2\2\2\u00c0")
+        buf.write("<\3\2\2\2\u00c1\u00c3\t\3\2\2\u00c2\u00c1\3\2\2\2\u00c3")
+        buf.write("\u00c4\3\2\2\2\u00c4\u00c2\3\2\2\2\u00c4\u00c5\3\2\2\2")
+        buf.write("\u00c5>\3\2\2\2\u00c6\u00c7\7\63\2\2\u00c7\u00c8\7)\2")
+        buf.write("\2\u00c8\u00c9\7d\2\2\u00c9@\3\2\2\2\u00ca\u00cc\t\4\2")
+        buf.write("\2\u00cb\u00ca\3\2\2\2\u00cc\u00cd\3\2\2\2\u00cd\u00cb")
+        buf.write("\3\2\2\2\u00cd\u00ce\3\2\2\2\u00ce\u00cf\3\2\2\2\u00cf")
+        buf.write("\u00d0\b!\2\2\u00d0B\3\2\2\2\7\2\u00af\u00bf\u00c4\u00cd")
+        buf.write("\3\b\2\2")
+        return buf.getvalue()
+
+
+class automata_verilogLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    T__3 = 4
+    T__4 = 5
+    T__5 = 6
+    T__6 = 7
+    T__7 = 8
+    T__8 = 9
+    T__9 = 10
+    T__10 = 11
+    T__11 = 12
+    T__12 = 13
+    T__13 = 14
+    T__14 = 15
+    T__15 = 16
+    T__16 = 17
+    T__17 = 18
+    T__18 = 19
+    T__19 = 20
+    T__20 = 21
+    T__21 = 22
+    MODULE_MARK = 23
+    END_MODULE_MARK = 24
+    NODE_TYPE = 25
+    ASSIGN_MARK = 26
+    INSTANCE_CONNECTION = 27
+    LINE_END = 28
+    NUM = 29
+    NAME = 30
+    BYTE_MARK = 31
+    WS = 32
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'('", "')'", "','", "'['", "':'", "']'", "'='", "'always'", 
+            "'@'", "'or'", "'posedge'", "'begin'", "'end'", "'if'", "'else'", 
+            "'=='", "'<='", "'~'", "'|'", "'&'", "'{'", "'}'", "'module'", 
+            "'endmodule'", "'assign'", "'.'", "';'", "'1'b'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "MODULE_MARK", "END_MODULE_MARK", "NODE_TYPE", "ASSIGN_MARK", 
+            "INSTANCE_CONNECTION", "LINE_END", "NUM", "NAME", "BYTE_MARK", 
+            "WS" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", 
+                  "T__7", "T__8", "T__9", "T__10", "T__11", "T__12", "T__13", 
+                  "T__14", "T__15", "T__16", "T__17", "T__18", "T__19", 
+                  "T__20", "T__21", "MODULE_MARK", "END_MODULE_MARK", "NODE_TYPE", 
+                  "ASSIGN_MARK", "INSTANCE_CONNECTION", "LINE_END", "NUM", 
+                  "NAME", "BYTE_MARK", "WS" ]
+
+    grammarFileName = "automata_verilog.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.7.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
